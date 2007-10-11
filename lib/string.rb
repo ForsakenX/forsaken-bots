77caa350b6bd09cd1cf099affa5a0e57b24c0f24
Daniel Aquino <mr.danielaquino@gmail.com>
2007-10-11 06:06:26 +0000

    updates

---
+++ lib/string.rb
@@ -0,0 +1,13 @@
+class String
+
+  # "FooBar".snake_case #=> "foo_bar"
+  def snake_case
+    gsub(/\B[A-Z]/, '_\&').downcase
+  end
+
+  # "foo_bar".camel_case #=> "FooBar"
+  def camel_case
+    split('_').map{|e| e.capitalize}.join
+  end
+
+end
