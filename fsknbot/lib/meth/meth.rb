2c89c257ba790486fa1ea376ab291fd520c5a22e
Daniel Aquino <mr.danielaquino@gmail.com>
2008-09-30 10:13:37 +0000

    updates

--- fsknbot/lib/meth/meth.rb
+++ fsknbot/lib/meth/meth.rb
@@ -1,21 +1,11 @@
 
-# meth requires irc module
 require "irc/irc"
 
-# meth module
 module Meth
 end
 
-#
 require 'meth/command'
 require 'meth/command_manager'
-
-# extends
-require 'meth/array'
-require 'meth/numeric'
-require 'meth/string'
-
-# meth parts
 require 'meth/plugin_manager'
 require 'meth/plugin'
 require 'meth/bot'
--- fsknbot/lib/meth/module.rb
+++
@@ -1,5 +0,0 @@
-module Module
-  def alias_methods new, targets
-    targets.each { |target| alias_method new, target }
-  end
-end
