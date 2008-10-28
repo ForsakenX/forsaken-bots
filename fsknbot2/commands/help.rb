7a8845eda0f0c0f7ec8fb439d5e176a58195e50d
Daniel Aquino <mr.danielaquino@gmail.com>
2008-10-28 16:49:43 +0000

    added help

---
+++ fsknbot2/commands/help.rb
@@ -0,0 +1,33 @@
+
+IrcCommandManager.register 'help' do |m|
+  m.reply HelpCommand.run(m.args[0])
+end
+
+class HelpCommand
+  class << self
+
+    def run command
+      help(command) || not_found
+    end
+
+    def help command='help'
+      (command||'help') == 'help' ? helphelp : IrcCommandManager.help[ command ]
+    end
+
+    def helphelp
+      "Help shows help on commands.  "+
+      "Syntax: help [command].  "+
+      "Commands: #{commands.join(', ')}"
+    end
+
+    def commands
+      IrcCommandManager.commands.keys
+    end
+
+    def not_found
+      "Command not found: #{commands.join(', ')}"
+    end
+
+  end
+end
+
