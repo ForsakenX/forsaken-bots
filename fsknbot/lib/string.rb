c4a11a04c511388044fbd7c17f8c12106e147149
Daniel Aquino <mr.danielaquino@gmail.com>
2008-10-31 08:51:50 +0000

    removing uneeded

--- fsknbot/lib/string.rb
+++ fsknbot/lib/string.rb
@@ -1,9 +1,5 @@
 class String
 
-=begin
-Case Helpers
-=end
-
   # "FooBar".snake_case #=> "foo_bar"
   def snake_case
     gsub(/\B[A-Z]/, '_\&').downcase
@@ -20,35 +16,6 @@ Case Helpers
 
   def camel_case!
     replace camel_case
-  end
-
-=begin
-Regex Helpers
-=end
-
-  def parse_regex
-    self =~ (/^\/(.+)\/$/m)
-    $1.nil? ? false : $1
-  end
-
-  def test_regex
-    begin
-      if (regex = parse_regex)===false
-        raise "String `#{self}' does not look like Regex"
-      end
-      Regexp.new(regex)
-      return true
-    rescue Exception => e
-      return e
-    end
-  end
-
-=begin
-Cleaners
-=end
-
-  def clean_ends
-    self.gsub(/^ +/,'').gsub(/ +$/,'')
   end
 
 end
