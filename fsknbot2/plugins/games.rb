da28cd6ebb4d1378a7f7a7439de7f17d4d914469
Daniel Aquino <mr.danielaquino@gmail.com>
2009-05-03 17:11:38 +0000

    updating

--- fsknbot2/plugins/games.rb
+++ fsknbot2/plugins/games.rb
@@ -6,41 +6,19 @@ class GamesCommand
   class << self
 
     def run
-
       return "No games...  " unless Game.length > 0
-    
-      hosts = []
-      waiting = []
+      output = []
       Game.games.each do |game|
-        if game.start_time
-          hosts << parse_host(game)
-        else
-          waiting << parse_waiting(game)
-        end
+	      time = seconds_to_clock( Time.now - game.start_time )
+	      output << "{ "+
+        	 "#{game.to_s} "+
+	         "started at: #{game.start_time.strftime('%I:%M:%S')} "+
+        	 "runtime #{time} "+
+	       "}"
       end
-    
-      output  = ""
-      output += "Hosting: #{hosts.join(', ')}" if hosts.length > 0
-      output += "Waiting: #{waiting.join(', ')}" if waiting.length > 0
-
-      output
+      "Games: #{output.join(', ')}"
     end
 
-    def parse_waiting game
-      time_left = game.timeout - (Time.now - game.created_at).to_i
-      time = seconds_to_clock(time_left)
-      "{ #{game.to_s} times out in: (#{time}) }"
-    end
-
-    def parse_host game
-      time = seconds_to_clock( Time.now - game.start_time )
-      "{ "+
-         "#{game.to_s} "+
-         "started at: #{game.start_time.strftime('%I:%M:%S')} "+
-         "runtime #{time} "+
-       "}"
-    end
-    
     def seconds_to_clock seconds
       seconds = seconds.to_i
       minutes = seconds / 60; seconds = seconds % 60
