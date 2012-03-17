000f5777bc041a1bf50e61d8a984842f53c2b12d
Daniel Aquino <mr.danielaquino@gmail.com>
2012-03-17 04:19:03 +0000

    updates

--- fsknbot2/start.sh
+++ fsknbot2/start.sh
@@ -2,5 +2,5 @@
 cd "`dirname "$0"`"
 ./run.rb 2>&1 >> log &
 pid=$!
-echo $pid > /home/aquinod/run/bot.pid
+echo $pid > ./run/bot.pid
 echo "started with pid $pid"
--- fsknbot2/stop.sh
+++ fsknbot2/stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-kill `cat /home/aquinod/run/bot.pid` || exit
+kill `cat ./run/bot.pid` || exit
 sleep 5
-kill `cat /home/aquinod/run/bot.pid` || exit
+kill `cat ./run/bot.pid` || exit
 sleep 5
-kill -9 `cat /home/aquinod/run/bot.pid`
-kill -9 `cat /home/aquinod/run/bot.pid`
-kill -9 `cat /home/aquinod/run/bot.pid`
-kill -9 `cat /home/aquinod/run/bot.pid`
+kill -9 `cat ./run/bot.pid`
+kill -9 `cat ./run/bot.pid`
+kill -9 `cat ./run/bot.pid`
+kill -9 `cat ./run/bot.pid`
