c16c152938fbaccf169fd91e3ee1093b4bf750b2
Daniel Aquino <mr.danielaquino@gmail.com>
2012-03-24 22:50:56 +0000

    printing user location when ip is requested

--- fsknbot2/plugins/ip.rb
+++ fsknbot2/plugins/ip.rb
@@ -1,6 +1,10 @@
 IrcCommandManager.register 'ip', "ip <user>: shows ip of user." do |m|
   if user = m.args.first.nil? ? m.from : IrcUser.find_by_nick(m.args.first)
-    m.reply "#{user.nick} => #{user.ip||user.host}"
+		if user.ip =~ /^unaffiliated/
+			m.reply "User #{user.nick} has hidden their ip..."
+		else
+			m.reply "#{user.nick} (#{user.ip}) #{user.location}"
+		end
   else
     m.reply "Unknown user"
   end
