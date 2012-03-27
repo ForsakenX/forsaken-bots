39e297c59f11dfdc12b3d54b2eb032869aa3c5a8
Daniel Aquino <mr.danielaquino@gmail.com>
2012-03-27 03:56:35 +0000

    limiting output of wolfram response and adding url to query

--- fsknbot2/plugins/wolfram.rb
+++ fsknbot2/plugins/wolfram.rb
@@ -4,7 +4,10 @@ IrcCommandManager.register ['knowledge','k','?'], "query wolfram alpha" do |m|
 		if query.empty?
 			m.reply "Missing input"
 		else
-			m.reply WolframAlpha.get_text_result(query)
+			output = WolframAlpha.get_text_result(query)
+			output = output.slice(/.{1,150}[^ ]{0,50}/m) if output.length > 150
+			output += " ... #{WolframAlpha.browser_url(query)}"
+			m.reply output
 		end
 	rescue Exception
 		puts_error __FILE__,__LINE__
@@ -19,22 +22,26 @@ class WolframAlpha
 class << self
 	APPID = File.read("#{ROOT}/config/wolfram.appid").strip
 	QUERY_URL = "http://api.wolframalpha.com/v1/query?appid=#{APPID}&input="
+	def browser_url input
+		"http://wolframalpha.com/input/?i=#{CGI.escape input}"
+	end
 	def query input
 		url = QUERY_URL + CGI.escape(input)
 		doc = Nokogiri::XML open(url)
 		success = doc.css('queryresult').first.attributes['success'].value == "true"
-		success = doc.css('queryresult').first.attributes['success'].value == "true"
 		didyoumean = doc.css('didyoumean').text
+		errormsg = doc.css('error msg').text
 		return [nil,"Did you mean? " + didyoumean] unless didyoumean.empty?
-		return [nil,doc.css('error msg').text] unless success
+		return [nil,"Error: " + errormsg] unless errormsg.empty?
+		return [nil,"Unknown Error"] unless success
 		doc
 	end
 	def get_text_result input
 		result,error = query(input)
 		return error if error
-		text = result.css('plaintext').map{|n|n.text.empty? ? nil : n.text}.compact.join('; ')
-		return "No plain text response available" if text.empty?
-		text
+		plaintexts = result.css('plaintext')
+		return "No plain text response available" if plaintexts.length == 0
+		plaintexts.map{|n| n.text }.join('; ')
 	end
 end
 end
