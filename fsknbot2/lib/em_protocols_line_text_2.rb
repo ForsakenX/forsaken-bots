de7324d8148b2935d5717a7e0c96323fa9455fcd
Daniel Aquino <mr.danielaquino@gmail.com>
2012-06-23 21:32:48 +0000

    adding time stamp to output lines

--- fsknbot2/lib/em_protocols_line_text_2.rb
+++ fsknbot2/lib/em_protocols_line_text_2.rb
@@ -2,7 +2,8 @@ module EM::Protocols::LineText2
 
   ## helper to send data with new line appended
   def send_line line
-    puts "irc <<< #{line}"
+    t=Time.now.strftime("%m-%d-%y %H:%M:%S")
+    puts "irc #{t} <<< #{line}"
     send_data "#{line}\n" unless line.nil?
   end
 
