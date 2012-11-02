deaba57c48e53526b7e06f663c38a75f48adbcc8
Daniel Aquino <mr.danielaquino@gmail.com>
2012-11-02 00:30:54 +0000

    tests for google plugin

---
+++ fsknbot2/tests/google_test_define.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + '/test.lib'
+require "google" # incase not executable
+
+query = "cars"
+
+puts GoogleCommand.define( query )
---
+++ fsknbot2/tests/google_test_desc.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__) + '/test.lib'
+require "google" # incase not executable
+
+puts GoogleCommand.desc( "cars" )
